7399f66825a2beab3627702e69868b2d5d5d5b95
Gustavo Trindade <gustavoferreiratrindade@gmail.com>
2023-02-03 14:39:07 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Start(6);
+        Menu();
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("S = Segundos => 10s = 10 segundos");
+            Console.WriteLine("M = Minutos => 1m = 1 minuto");
+            Console.WriteLine("0 = Sair");
+            Console.WriteLine("Quanto tempo deseja contar?");
+
+            string data = Console.ReadLine().ToLower();
+
+            //Pegar o último elemento
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+
+            Console.WriteLine(data);
+            Console.WriteLine(type);
+        }
+
+
+        static void Start(int time)
+        {
+            int currentTime = 0;
+
+            while (currentTime != time)
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(500);
+            }
+
+            Console.Clear();
+            Console.WriteLine("Stopwatch finalizado");
+            Thread.Sleep(2500);
+        }
+    }
+}
